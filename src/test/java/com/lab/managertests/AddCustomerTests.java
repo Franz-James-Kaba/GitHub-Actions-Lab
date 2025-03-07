3cf57681e0b2a362fe53f9382fd3d3d177e99718
Franz-James-Kaba <franz-james.kaba@amalitech.com>
2025-03-07 08:32:28 +0000

    This is the commit of a changes to the add customer test

--- src/test/java/com/lab/managertests/AddCustomerTests.java
+++ src/test/java/com/lab/managertests/AddCustomerTests.java
@@ -21,35 +21,35 @@ public class AddCustomerTests extends Setup {
         addCustomer.clickSubmitButton();
     }
 
-    @Test
-    public void testFailAddCustomerCase2(){
-        addCustomer.setFirstNameField("Joker123");
-        addCustomer.setLastNameField("sga^gst");
-        addCustomer.setPostalCodeField("0729354");
-        addCustomer.clickSubmitButton();
-    }
-
-    @Test
-    public void testFailAddCustomerCase3(){
-        addCustomer.setFirstNameField("Joker123");
-        addCustomer.setLastNameField("sga4543");
-        addCustomer.setPostalCodeField("0729354");
-        addCustomer.clickSubmitButton();
-    }
-    @Test
-    public void testFailAddCustomerCase4(){
-        addCustomer.setFirstNameField("$^$^#");
-        addCustomer.setLastNameField("*&%$");
-        addCustomer.setPostalCodeField("0729354");
-        addCustomer.clickSubmitButton();
-    }
-    @Test
-    public void testFailAddCustomerCase5(){
-        addCustomer.setFirstNameField("Titan");
-        addCustomer.setLastNameField("Killer");
-        addCustomer.setPostalCodeField("5926437ABC0");
-        addCustomer.clickSubmitButton();
-    }
+//    @Test
+//    public void testFailAddCustomerCase2(){
+//        addCustomer.setFirstNameField("Joker123");
+//        addCustomer.setLastNameField("sga^gst");
+//        addCustomer.setPostalCodeField("0729354");
+//        addCustomer.clickSubmitButton();
+//    }
+//
+//    @Test
+//    public void testFailAddCustomerCase3(){
+//        addCustomer.setFirstNameField("Joker123");
+//        addCustomer.setLastNameField("sga4543");
+//        addCustomer.setPostalCodeField("0729354");
+//        addCustomer.clickSubmitButton();
+//    }
+//    @Test
+//    public void testFailAddCustomerCase4(){
+//        addCustomer.setFirstNameField("$^$^#");
+//        addCustomer.setLastNameField("*&%$");
+//        addCustomer.setPostalCodeField("0729354");
+//        addCustomer.clickSubmitButton();
+//    }
+//    @Test
+//    public void testFailAddCustomerCase5(){
+//        addCustomer.setFirstNameField("Titan");
+//        addCustomer.setLastNameField("Killer");
+//        addCustomer.setPostalCodeField("5926437ABC0");
+//        addCustomer.clickSubmitButton();
+//    }
 
 
 }
